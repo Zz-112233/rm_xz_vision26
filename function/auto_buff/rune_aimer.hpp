cbc2d601beee0488bdbda18636b9db802e73167d
Chaichai438 <1939327800@qq.com>
2025-11-20 19:02:32 +0800

    能量机关更新

---
+++ function/auto_buff/rune_aimer.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace auto_buff
+{
+    class
+}
---
+++ function/auto_buff/rune_detector.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+namespace auto_buff
+{
+  class RuneDetection
+  {
+  public:
+    RuneDetection(const std::string& config);
+
+  private:
+  }
+} // namespace auto_buff
